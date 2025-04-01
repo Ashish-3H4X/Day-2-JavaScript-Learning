526c7eb5138e9181f8aef73e1ee19bd72dca69fa
Mr Rahul <ashish227087@gmail.com>
2025-04-01 21:20:01 +0530

    adding Assignment opertors

--- project1/script.js
+++ project1/script.js
@@ -1,24 +1,24 @@
- // example 1
+// example 1
 
- var x= 3;
- var y = 4;
- x += y;
- document.getElementById('demo1').innerHTML=`
+var x = 3;
+var y = 4;
+x += y;
+document.getElementById('demo1').innerHTML = `
  var x= 3;<br>
  var y = 4;<br>
  x += y; <br>
  result =
 ${x}
  `
- // 7
+// 7
 
 
 // example 2
 
- var text1= "Hello";
- var text2 = "ASHISH 3H4X";
-  text1 += text2;
- document.getElementById('demo2').innerHTML=`
+var text1 = "Hello";
+var text2 = "ASHISH 3H4X";
+text1 += text2;
+document.getElementById('demo2').innerHTML = `
   var text1= "Hello "; <br>
  var text2 = "ASHISH 3H4X"; <br>
   text1 += text2;<br>
@@ -29,10 +29,10 @@ ${text1}`
 
 // example 3 
 
-var x= 3;
- var y = 4;
- x -= y;
- document.getElementById('demo3').innerHTML=`
+var x = 3;
+var y = 4;
+x -= y;
+document.getElementById('demo3').innerHTML = `
  var x= 3;<br>
  var y = 4;<br>
  x -= y; <br>
@@ -42,10 +42,10 @@ ${x}
 
 
 // example 4 *= operator
-var x= 3;
- var y = 4;
- x *= y;
- document.getElementById('demo4').innerHTML=`
+var x = 3;
+var y = 4;
+x *= y;
+document.getElementById('demo4').innerHTML = `
  var x= 3;<br>
  var y = 4;<br>
  x *= y; <br>
@@ -55,10 +55,10 @@ ${x}
 
 
 // example 5 **= operator
-var x= 3;
- var y = 4;
- x **= y;
- document.getElementById('demo5').innerHTML=`
+var x = 3;
+var y = 4;
+x **= y;
+document.getElementById('demo5').innerHTML = `
  var x= 3;<br>
  var y = 4;<br>
  x **= y; <br>
@@ -67,10 +67,10 @@ ${x}
 `
 // example 6 =/operator
 
-var x= 3;
- var y = 4;
- x /= y;
- document.getElementById('demo6').innerHTML=`
+var x = 3;
+var y = 4;
+x /= y;
+document.getElementById('demo6').innerHTML = `
  var x= 3;<br>
  var y = 4;<br>
  x /= y; <br>
@@ -79,10 +79,10 @@ ${x}
 `
 // example 7 %= operator
 
-var x= 3;
- var y = 4;
- x %= y;
- document.getElementById('demo7').innerHTML=`
+var x = 3;
+var y = 4;
+x %= y;
+document.getElementById('demo7').innerHTML = `
  var x= 3;<br>
  var y = 4;<br>
  x %= y; <br>
---
+++ project2/script.js
@@ -0,0 +1,52 @@
+// example 1
+var x = 10;
+var y = 5;
+x <<= y;
+document.getElementById('demo1').innerHTML = `
+ var x= 3;<br>
+ var y = 4;<br>
+ x <<= y; <br>
+ result =
+${x}
+ `
+ // 48
+
+ // example 2
+ var x = 3;
+var y = 4;
+x >>= y;
+document.getElementById('demo2').innerHTML = `
+ var x= 3;<br>
+ var y = 4;<br>
+ x >>= y; <br>
+ result =
+${x}
+ `
+ // 0
+ 
+ // example 3
+ var x = -100;
+ var y = 5;
+ x>>>=y;
+ document.getElementById('demo3').innerHTML=`
+ var x = -100; <br>
+var y = 5;<br>
+x>>>=y;<br>
+result =
+${x}
+ `
+//  134217724
+// example 4
+
+var x = 10 ;
+var y = 5;
+x &= y;
+document.getElementById('demo4').innerHTML=`
+
+var x = 10 ;<br>
+var y = 9;<br>
+x &= y;<br>
+result =
+${x}
+
+`
